c0017b60ad9ca44c744b329db7785f9f8da764c5
Richard White <richard.white@sanger.ac.uk>
2018-04-17 20:25:44 +0100

    Create theme_bubble()

--- R/bubble_plot.R
+++ R/bubble_plot.R
@@ -60,27 +60,11 @@ bubble_plot <- function(plot_df, x = 'x', y = 'y', size = 'size',
                                 class(plot_df[[y]]) == 'factor',
                               TRUE, FALSE)
   # create theme
-  if (x_is_categorical | y_is_categorical) {
-    bubble_theme <- ggplot2::theme_void() +
-      ggplot2::theme(
-        axis.text.x = ggplot2::element_text(size = 12, colour = 'black', angle = 90,
-                                            hjust = 0, debug = FALSE),
-        axis.text.y = ggplot2::element_text(size = 12, colour = 'black', angle = 0,
-                                            hjust = 0, debug = FALSE),
-        panel.grid.major = ggplot2::element_line(colour = 'grey80', linetype = 'dotted'),
-        legend.position = 'top',
-        legend.title = ggplot2::element_text(size = 14),
-        legend.text = ggplot2::element_text(size = 12)
-      )
-  } else {
-    bubble_theme <- ggplot2::theme_minimal()
-  }
-
   bubble_plot <- ggplot2::ggplot(data = plot_df) +
     ggplot2::geom_point(ggplot2::aes_(x = as.name(x), y = as.name(y), size = as.name(size),
                     fill = as.name(fill)), shape = 21, ... ) +
     viridis::scale_fill_viridis(direction = -1) +
-    bubble_theme
+    theme_bubble(base_size = 12, categorical = (x_is_categorical | y_is_categorical) )
 
   if (x_is_categorical) {
     if (!is.null(x_labels) ) {
@@ -100,4 +84,54 @@ bubble_plot <- function(plot_df, x = 'x', y = 'y', size = 'size',
   }
 
   return(bubble_plot)
+}
+
+
+#' A ggplot theme for bubble plots
+#'
+#' \code{theme_bubble} produces a ggplot bubble plot from a data.frame
+#'
+#' This is a ggplot theme for bubble plots.
+#' For categorical axes, it is theme_void with x and y axis labels
+#' It also has grey dotted grid lines and the legend is place at the top
+#'
+#' If both x and y variables are continuous, theme_minimal is returned
+#'
+#' \code{bubble_plot} uses this theme internally
+#'
+#' @param base_size      numeric    base size of text in points (default: 11)
+#' @param base_family    character  base font family (default: "")
+#' @param categorical    logical    are either of the x and y variable categorical (default: TRUE)
+#'
+#' @return ggplot theme object
+#'
+#' @examples
+#'
+#' set.seed(20962)
+#' test_data_cat <- data.frame(x = factor(rep(LETTERS[1:10], 10)),
+#'   y = factor(rep(1:10, each = 10), levels = 10:1),
+#'   size = sample(20:100, 100, replace = TRUE),
+#'   fill = runif(100))
+#'
+#' cat_bubble_plot <- bubble_plot(test_data_cat)
+#' cat_bubble_plot + theme_bubble() + theme(axis.text.x = element_text(angle = 0, face = 'italic'))
+#'
+#' @export
+theme_bubble <- function(base_size = 11, base_family = "", categorical = TRUE) {
+  if (categorical) {
+    ggplot2::theme_void(base_size = base_size, base_family = base_family) +
+      ggplot2::theme(
+        axis.text =
+          ggplot2::element_text(family = "", face = "plain", colour = 'black',
+                                size = base_size, hjust = 0, vjust = 0, angle = 0,
+                                lineheight = 0.9, debug = FALSE),
+        axis.text.x = ggplot2::element_text(angle = 90),
+        panel.grid.major = ggplot2::element_line(colour = 'grey80', linetype = 'dotted'),
+        legend.position = 'top',
+        legend.title = ggplot2::element_text(size = base_size + 2),
+        legend.text = ggplot2::element_text(size = base_size)
+      )
+  } else {
+    ggplot2::theme_minimal()
+  }
 }
