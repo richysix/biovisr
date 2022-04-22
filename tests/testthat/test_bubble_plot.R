e18723566c1ae36b0bcfbbdb8eca91a7233a36d3
Richard White <richard.white@qmul.ac.uk>
2022-04-22 11:54:02 +0100

    Change bubble plot tests to using snapshots

--- tests/testthat/test_bubble_plot.R
+++ tests/testthat/test_bubble_plot.R
@@ -1,5 +1,5 @@
-context('bubble plot')
 library(biovisr)
+library(vdiffr)
 
 set.seed(20229)
 
@@ -12,24 +12,27 @@ test_data_num <- data.frame(x = runif(100, min = 1, max = 50),
                             y = runif(100, min = 1, max = 50),
                             size = sample(20:100, 100, replace = TRUE),
                             fill = runif(100))
-
+#cat('got here 1\n')
 cat_bubble_plot <- bubble_plot(test_data_cat)
 num_bubble_plot <- bubble_plot(test_data_num)
 
 # reverse y factor levels
 test_data_cat$y <- factor(test_data_cat$y, levels = 10:1)
+#cat('got here 2\n')
 cat_bubble_plot_rev <- bubble_plot(test_data_cat)
 
+cat('got here 3\n')
 test_that('categorical plot', {
-  expect_known_value(cat_bubble_plot, 'cat_bubble_plot.rds')
+  expect_doppelganger("categorical bubble plot", cat_bubble_plot)
 })
 
+# cat('got here 4\n')
 test_that('continuous plot', {
-  expect_known_value(num_bubble_plot, 'num_bubble_plot.rds')
+  expect_doppelganger("continuous bubble plot", num_bubble_plot)
 })
 
+# cat('got here 5\n')
 test_that('categorical plot, y reversed', {
-  expect_known_value(cat_bubble_plot_rev,
-                      'cat_bubble_plot_rev.rds')
+  expect_doppelganger("categorical plot, y reversed", cat_bubble_plot_rev)
 })
 
